e421d11bd362de537b8ab04438865010897256d7
Geremias Grassi <geremias.grassi@gmail.com>
2025-05-06 11:37:10 -0300

    Carousel hecho

---
+++ js/klee-carousel.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const kleeCarousel = document.querySelector(".klee-swiper");
+
+  if (kleeCarousel) {
+    new Swiper(".klee-swiper", {
+      loop: true,
+      slidesPerView: 1,
+      spaceBetween: 30,
+      autoplay: {
+        delay: 3000,          // 3 segundos
+        disableOnInteraction: false
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      }
+    });
+  }
+});
