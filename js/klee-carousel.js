2ef14430edabf44f861d95188870e04f313e6ae3
Geremias Grassi <geremias.grassi@gmail.com>
2025-05-08 13:59:19 -0300

    Nuevo carousel mejorado mobile

--- js/klee-carousel.js
+++ js/klee-carousel.js
@@ -1,19 +1,19 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const kleeCarousel = document.querySelector(".klee-swiper");
+// document.addEventListener("DOMContentLoaded", function () {
+//   const kleeCarousel = document.querySelector(".klee-swiper");
 
-  if (kleeCarousel) {
-    new Swiper(".klee-swiper", {
-      loop: true,
-      slidesPerView: 1,
-      spaceBetween: 0,
-      autoplay: {
-        delay: 3000,          // 3 segundos
-        disableOnInteraction: false
-      },
-      navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-      }
-    });
-  }
-});
+//   if (kleeCarousel) {
+//     new Swiper(".klee-swiper", {
+//       loop: true,
+//       slidesPerView: 1,
+//       spaceBetween: 0,
+//       autoplay: {
+//         delay: 3000,          // 3 segundos
+//         disableOnInteraction: false
+//       },
+//       navigation: {
+//         nextEl: ".swiper-button-next",
+//         prevEl: ".swiper-button-prev",
+//       }
+//     });
+//   }
+// });
